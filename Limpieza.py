11fcff7a2b4955aae6656af1dd24d40eaab7d5f6
Brandon Islas <brandon98642@gmail.com>
2020-10-12 20:16:43 -0500

    Limpieza

--- Limpieza.py
+++ Limpieza.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 dataframe_general= pd.read_csv('visita19.csv')
 predataframe1=dataframe_general.iloc [:, [1,4,5,11,13,17,18,19,21]]
-predaraframe2=dataframe_general.iloc [:, 23:65] 
+predaraframe2=dataframe_general.iloc [:, 23:66] 
 dataframe_seleccion= pd.concat([predataframe1,predaraframe2], axis=1)
 aux=dataframe_seleccion['VISIT_ANIO,C,2'].dropna()
 dataframe_seleccion['VISIT_ANIO,C,2'].fillna(round(aux.mean()), inplace=True)
@@ -19,6 +19,8 @@ dataframe_seleccion['MENORES_12,C,3'].fillna(round(aux.mean()), inplace=True)
 #Para NIV_APREND
 aux=dataframe_seleccion['NIV_APREND,C,2'].dropna()
 dataframe_seleccion['NIV_APREND,C,2'].fillna(round(aux.mean()), inplace=True)
-print(dataframe_seleccion.isnull().sum())
+#print(dataframe_seleccion.isnull().sum())
+dataframeDatosDescriptivos= dataframe_seleccion.describe()
+print(dataframeDatosDescriptivos)
 
 
