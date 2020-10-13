0a4f2ab74d2845a7bc9b1ff4f0e81b278a81a1ec
Brandon Islas <brandon98642@gmail.com>
2020-10-12 19:49:14 -0500

    Limpieza

---
+++ Limpieza.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+dataframe_general= pd.read_csv('visita19.csv')
+predataframe1=dataframe_general.iloc [:, [1,4,5,11,13,17,18,19,21]]
+predaraframe2=dataframe_general.iloc [:, 23:65] 
+dataframe_seleccion= pd.concat([predataframe1,predaraframe2], axis=1)
+aux=dataframe_seleccion['VISIT_ANIO,C,2'].dropna()
+dataframe_seleccion['VISIT_ANIO,C,2'].fillna(round(aux.mean()), inplace=True)
+#Para MEDIO_2
+aux=dataframe_seleccion['MEDIO_2,C,2'].dropna()
+dataframe_seleccion['MEDIO_2,C,2'].fillna(round(aux.mean()), inplace=True)
+#Para TAM_GRUPO
+aux=dataframe_seleccion['TAM_GRUPO,C,3'].dropna()
+dataframe_seleccion['TAM_GRUPO,C,3'].fillna(round(aux.mean()), inplace=True)
+#Para MENORES
+aux=dataframe_seleccion['MENORES_12,C,3'].dropna()
+dataframe_seleccion['MENORES_12,C,3'].fillna(round(aux.mean()), inplace=True)
+#Para NIV_APREND
+aux=dataframe_seleccion['NIV_APREND,C,2'].dropna()
+dataframe_seleccion['NIV_APREND,C,2'].fillna(round(aux.mean()), inplace=True)
+print(dataframe_seleccion.isnull().sum())
+
+
